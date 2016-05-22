a939472d6e68b982a9d51ab5f04e1333a6b14e52
wb-yangxiaowei <wb-yangxiaowei@alibaba-inc.com>
2016-05-22 21:11:39 +0800

    update

--- src/components/GalleryByReactApp.js
+++ src/components/GalleryByReactApp.js
@@ -4,14 +4,14 @@ var React = require('react/addons');
 
 // CSS
 require('normalize.css');
-require('../styles/main.css');
+require('../styles/main.scss');
 
 // 获取图片相关数据
 var imageDatas = require('../data/imageDatas.json');
 
 //将图片名信息转成图片URL
 imageDatas = (function genImageURL(imageDatasArr) {
-  for (var i = 0, j=imageDatasArr.length; i < j; i++) {
+  for (var i = 0, j = imageDatasArr.length; i < j; i++) {
     var singleImageData = imageDatasArr[i];
 
     singleImageData.imageURL = require('../images/' + singleImageData.fileName);
@@ -25,12 +25,12 @@ imageDatas = (function genImageURL(imageDatasArr) {
 var GalleryByReactApp = React.createClass({
   render: function() {
     return (
-      <section className="stage">
-        <section className="img-sec">
+        <section className="stage" ref="stage">
+            <section className="img-sec">
+            </section>
+            <nav className="controller-nav">
+            </nav>
         </section>
-        <nav className="controller-nav">
-        </nav>
-      </section>
     );
   }
 });
